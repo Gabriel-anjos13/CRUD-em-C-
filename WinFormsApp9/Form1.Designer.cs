7cb7e01b774ea9882bcf43f377cdb688be9043bc
Aluno <Aluno@SA-LAB05-34>
2024-10-24 20:44:26 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp9/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WinFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvProdutos = new DataGridView();
+            btnAdicionar = new Button();
+            btnExcluir = new Button();
+            btnEditar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvProdutos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvProdutos
+            // 
+            dgvProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProdutos.Location = new Point(68, 29);
+            dgvProdutos.Name = "dgvProdutos";
+            dgvProdutos.RowTemplate.Height = 25;
+            dgvProdutos.Size = new Size(677, 214);
+            dgvProdutos.TabIndex = 0;
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.Location = new Point(68, 290);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(163, 76);
+            btnAdicionar.TabIndex = 1;
+            btnAdicionar.Text = "ADICIONAR PRODUTO";
+            btnAdicionar.UseVisualStyleBackColor = true;
+            btnAdicionar.Click += btnAdicionar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.Location = new Point(582, 290);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(163, 76);
+            btnExcluir.TabIndex = 2;
+            btnExcluir.Text = "EXCLUIR PRODUTO";
+            btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(325, 290);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(163, 76);
+            btnEditar.TabIndex = 3;
+            btnEditar.Text = "EDITAR PRODUTO";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEditar);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnAdicionar);
+            Controls.Add(dgvProdutos);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvProdutos).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvProdutos;
+        private Button btnAdicionar;
+        private Button btnExcluir;
+        private Button btnEditar;
+    }
+}
---
+++ WinFormsApp9/Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WinFormsApp9
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNome = new TextBox();
+            txtPreco = new TextBox();
+            lblNome = new Label();
+            lblPreco = new Label();
+            btnSalvar = new Button();
+            SuspendLayout();
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(251, 80);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(287, 23);
+            txtNome.TabIndex = 0;
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(251, 154);
+            txtPreco.Name = "txtPreco";
+            txtPreco.Size = new Size(287, 23);
+            txtPreco.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lblNome.Location = new Point(130, 80);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(70, 28);
+            lblNome.TabIndex = 2;
+            lblNome.Text = "NOME";
+            // 
+            // lblPreco
+            // 
+            lblPreco.AutoSize = true;
+            lblPreco.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPreco.Location = new Point(130, 149);
+            lblPreco.Name = "lblPreco";
+            lblPreco.Size = new Size(71, 28);
+            lblPreco.TabIndex = 3;
+            lblPreco.Text = "PREÇO";
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(251, 266);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(287, 83);
+            btnSalvar.TabIndex = 4;
+            btnSalvar.Text = "SALVAR";
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalvar);
+            Controls.Add(lblPreco);
+            Controls.Add(lblNome);
+            Controls.Add(txtPreco);
+            Controls.Add(txtNome);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNome;
+        private TextBox txtPreco;
+        private Label lblNome;
+        private Label lblPreco;
+        private Button btnSalvar;
+    }
+}
